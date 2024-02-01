df0dffd1e031ab22071571ede2a6eb779df2eeca
USER-PC <USER-PC@ANGEL>
2024-02-01 18:47:15 -0500

    Add project files.

---
+++ Ogechi_Ikediashi-PROG8051_Virtual-Pet-Simulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogechi_Ikediashi_PROG8051_Virtual_Pet_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Committing my first code to Git
+
+
+
+
+        }
+    }
+}
